99061c2658e62614bcfd768a7c8cefa373473262
Fabien <fabien.mixtur@gmail.com>
2025-07-01 15:04:58 +0200

    correction

--- player/player.h
+++ player/player.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmixtur <fmixtur@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/30 18:09:15 by fmixtur           #+#    #+#             */
-/*   Updated: 2025/06/30 18:09:18 by fmixtur          ###   ########.ch       */
+/*   Created: 2025/07/01 15:03:47 by fmixtur           #+#    #+#             */
+/*   Updated: 2025/07/01 15:04:26 by fmixtur          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ typedef struct s_ray
 	float	ray_x;
 	float	ray_y;
 	float	distance;
-} t_ray;
+}	t_ray;
 
-int		find_and_init_player(char **map, int cell_size, t_game *game);
+int		player_find_and_init(char **map, int cell_size, t_game *game);
 int		is_wall_collision(float new_x, float new_y, t_game *game);
-float   raycast_hit(t_game *game, t_ray *ray, float *hit_x, float *hit_y);
-void    draw_single_ray(t_game *game, t_ray *ray);
-void    cast_rays_90_degrees(t_game *game);
+float	raycast_hit(t_game *game, t_ray *ray, float *hit_x, float *hit_y);
+void	draw_single_ray(t_game *game, t_ray *ray);
+void	cast_rays_90_degrees(t_game *game);
 
 #endif
