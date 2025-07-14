50eed0ff9caaa094bb61ac123c02c0ad3011aaaa
Fabien <fabien.mixtur@gmail.com>
2025-07-15 00:26:48 +0200

    SAVE

--- render/render.c
+++ render/render.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmixtur <fmixtur@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/11 15:25:07 by fmixtur           #+#    #+#             */
-/*   Updated: 2025/07/11 15:25:58 by fmixtur          ###   ########.ch       */
+/*   Created: 2025/07/14 23:24:31 by fmixtur           #+#    #+#             */
+/*   Updated: 2025/07/14 23:50:21 by fmixtur          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,79 +22,75 @@ static void	put_pixel(t_img *img, int x, int y, int color)
 	*(unsigned int *)dest = color;
 }
 
-void	draw_square(int x, int y, t_game *game, int size, int color)
+static void	draw_vertical_line(int x, int y1, int y2, t_game *game, int color)
 {
-	int	i;
-	int	j;
+	int y;
+	int y_start;
+	int y_end;
 
-	i = 0;
-	while (i < size)
+	if (y1 < y2)
 	{
-		j = 0;
-		while (j < size)
-		{
-			put_pixel(&game->img, x + i, y + j, color);
-			j++;
-		}
-		i++;
+		y_start = y1;
+		y_end = y2;
+	}
+	else
+	{
+		y_start = y2;
+		y_end = y1;
+	}
+	y = y_start;
+	while (y <= y_end)
+	{
+		put_pixel(&game->img, x, y, color);
+		y++;
+	}
+}
+
+static void	draw_horizontal_line(int x1, int x2, int y, t_game *game, int color)
+{
+	int x;
+	int x_start;
+	int x_end;
+
+	if (x1 < x2)
+	{
+		x_start = x1;
+		x_end = x2;
+	}
+	else
+	{
+		x_start = x2;
+		x_end = x1;
+	}
+	x = x_start;
+	while (x <= x_end)
+	{
+		put_pixel(&game->img, x, y, color);
+		x++;
 	}
 }
 
 void	draw_line(int x1, int y1, int x2, int y2, t_game *game, int color)
 {
-	int	dx = abs(x2 - x1);
-	int	dy = abs(y2 - y1);
-	int	sx = x1 < x2 ? 1 : -1;
-	int	sy = y1 < y2 ? 1 : -1;
-	int	err = dx - dy;
-	int	x = x1;
-	int	y = y1;
-	int	e2;
-
-	while (1)
-	{
-		if (x >= 0 && x < WINDOW_WIDTH && y >= 0 && y < WINDOW_HEIGHT)
-			put_pixel(&game->img, x, y, color);
-		if (x == x2 && y == y2)
-			break ;
-		e2 = 2 * err;
-		if (e2 > -dy)
-		{
-			err -= dy;
-			x += sx;
-		}
-		if (e2 < dx)
-		{
-			err += dx;
-			y += sy;
-		}
-	}
+	if (x1 == x2)
+		draw_vertical_line(x1, y1, y2, game, color);
+	else if (y1 == y2)
+		draw_horizontal_line(x1, x2, y1, game, color);
 }
 
 void	draw_background(t_game *game)
 {
-	int	x;
-	int	y;
+	int y;
 
 	y = 0;
 	while (y < WINDOW_HEIGHT / 2)
 	{
-		x = 0;
-		while (x < WINDOW_WIDTH)
-		{
-			put_pixel(&game->img, x, y, game->config.ceiling_color);
-			x++;
-		}
+		draw_horizontal_line(0, WINDOW_WIDTH - 1, y, game, game->config.ceiling_color);
 		y++;
 	}
 	while (y < WINDOW_HEIGHT)
 	{
-		x = 0;
-		while (x < WINDOW_WIDTH)
-		{
-			put_pixel(&game->img, x, y, game->config.floor_color);
-			x++;
-		}
+		draw_horizontal_line(0, WINDOW_WIDTH - 1, y, game, game->config.floor_color);
 		y++;
 	}
 }
@@ -120,6 +116,24 @@ void	draw_map(t_game *game)
 			else
 				draw_square(j * game->cell_size, i * game->cell_size,
 					game, game->cell_size, 0x444444);
+			j++;
+		}
+		i++;
+	}
+}
+
+void draw_square(int x, int y, t_game *game, int size, int color)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			put_pixel(&game->img, x + i, y + j, color);
 			j++;
 		}
 		i++;
